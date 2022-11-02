010befac8090ce7d81f66a164e6dbbd0d1c1d0cb
SequelToCorn <90779926+SequelToCorn@users.noreply.github.com>
2022-11-02 18:13:10 -0400

    Add files via upload

--- OmniCalc.m
+++ OmniCalc.m
@@ -1,1 +1,21 @@
-% good evening this is a test
+%% Constants
+
+R = 8314; % universal gas constant, J/(mol*K)
+
+%Environmental Constants
+
+g = 9.81; % acceleration due to Earth's gravity, m/s    
+P0 = 101325; % atmospheric pressure at sea level, Pa
+
+R_air = 287.058; % gas constant of air, J/(kg*K)
+L = 0.00976; % temperature Lapse rate of air, K/m
+M = 0.02896968; % molar mass of air, kg/mol
+
+%Rocket Constants
+
+R_combust = 119.2; % gas constant of the motor combustion products, J/(kg*K)
+T_combust = 1837.2; % temperature of the motor combustion prodects, K
+
+
+%% Vehicle Parameters
+number_of_bees = 10000;
