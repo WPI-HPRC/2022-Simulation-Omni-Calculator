319b431ecaeb2631ac5bacf14401fdf95a770098
csmcafee <116767070+csmcafee@users.noreply.github.com>
2022-11-04 17:17:56 -0400

    Update OmniCalc.m

--- OmniCalc.m
+++ OmniCalc.m
@@ -1,8 +1,8 @@
 
 %% Rocket Constants
 
-R_combust = 356; % gas constant of the motor combustion products, J/(kg*K)
-T_combust = 2773; % temperature of the motor combustion prodects, K
+R_combust = 356; % gas constant of the motor combustion products, J/(kg*K) %%TODO
+T_combust = 2773; % temperature of the motor combustion prodects, K %%TODO
 
 % I love bees
 
@@ -11,47 +11,47 @@ T_combust = 2773; % temperature of the motor combustion prodects, K
 
 number_of_bees = 10000;
 
-airframe_diameter = 6; % Airframe Diameter (in)
-recovery_bay_length = 30; % Recovery Bay Length (in)
-coupler_length = 6; % Coupler Length (in)
-shock_cord_length = 400; % Length of Shock Cord (in)
+airframe_diameter = 6; % Airframe Diameter (in) %%TODO
+recovery_bay_length = 30; % Recovery Bay Length (in) %%TODO
+coupler_length = 6; % Coupler Length (in) %%TODO
+shock_cord_length = 400; % Length of Shock Cord (in) %%TODO
 
-lower_mass = 38; % Lower Section Mass (Ib)
-upper_mass = 0.01; % Upper Section Mass (Ib)
+lower_mass = 38; % Lower Section Mass (Ib) %%TODO
+upper_mass = 0.01; % Upper Section Mass (Ib) %%TODO
 
-drouge_diameter = 36; % Drouge Chute Diameter (in)
-packed_drouge_diameter = 2.85; % Packed Drouge Chute Diameter (in)
-drouge_n = 2.5; % Drogue Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6)
-lower_main_diameter = 144; % Lower Main Chute Diameter (in)
-packed_lower_main_diameter = 6; % Packed Lower Main Chute Diameter (in)
-lower_main_n = 2.5; % Lower Main Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6)
-upper_main_diameter = 144; % Upper Main Chute Diameter (in)
-packed_upper_main_diameter = 6; % Packed Upper Main Chute Diameter (in)
-upper_main_n = 2.5; % Upper Main Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6)
+drouge_diameter = 36; % Drouge Chute Diameter (in) %%TODO
+packed_drouge_diameter = 2.85; % Packed Drouge Chute Diameter (in) %%TODO
+drouge_n = 2.5; % Drogue Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6) %%TODO
+lower_main_diameter = 144; % Lower Main Chute Diameter (in) %%TODO
+packed_lower_main_diameter = 6; % Packed Lower Main Chute Diameter (in) %%TODO
+lower_main_n = 2.5; % Lower Main Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6) %%TODO
+upper_main_diameter = 144; % Upper Main Chute Diameter (in) %%TODO
+packed_upper_main_diameter = 6; % Packed Upper Main Chute Diameter (in) %%TODO
+upper_main_n = 2.5; % Upper Main Chute Canopy Fill Constant (see Parachute Recovery Systems Design Manual, Knacke, Table 5-6) %%TODO
 
-fin_area = 5.11; % Frontal Fin Area (in^2)
+fin_area = 5.11; % Frontal Fin Area (in^2) %%TODO
 
-cd_parachute = 2.2; % Parachute Coeffient of Drag
-cd_lower = 0.32; % Lower Section Coeffient of Drag
-cd_upper = 0.32; % Upper Section Coeffient of Drag
-cd_fin = 0.09; % Fin Coeffient of Drag
+cd_parachute = 2.2; % Parachute Coeffient of Drag %%TODO
+cd_lower = 0.32; % Lower Section Coeffient of Drag %%TODO
+cd_upper = 0.32; % Upper Section Coeffient of Drag %%TODO
+cd_fin = 0.09; % Fin Coeffient of Drag %%TODO
 
 
 %% Flight parameters
 
-burnout_AGL = 1477; % predicted burnout altitude above ground level, ft
-apogee_AGL = 4099; % predicted highest point of flight above ground level, ft
-main_AGL = 600; % predectied altitude above ground level, ft
+burnout_AGL = 1477; % predicted burnout altitude above ground level, ft %%TODO
+apogee_AGL = 4099; % predicted highest point of flight above ground level, ft %%TODO
+main_AGL = 600; % predectied altitude above ground level, ft %%TODO
 
-Max_Vel = 58; % the predicted maximum velocity of the rocket, ft
-Max_drift = 2500; % maximum allowable drift, ft
+Max_Vel = 58; % the predicted maximum velocity of the rocket, ft %%TODO
+Max_drift = 2500; % maximum allowable drift, ft %%TODO
 
 
 %% Launch Site Parameters
 
-launch_MSL = 5700; % altitude of the launch site above mean sea level, ft
-temperature = 90; % ambient temperature of the launch site, F
-max_wind_vel = 20; % maximum allowable wind speed, mph
+launch_MSL = 5700; % altitude of the launch site above mean sea level, ft %%TODO
+temperature = 90; % ambient temperature of the launch site, F %%TODO
+max_wind_vel = 20; % maximum allowable wind speed, mph %%TODO
 
 
 %% Constants
@@ -98,4 +98,6 @@ Max_drift = Max_drift*0.3048; % maximum allowable drift, (m)
 launch_MSL = launch_MSL*0.3048; % altitude of the launch site above mean sea level, (m)
 temperature = (5/9)*(temperature-32); % ambient temperature of the launch site, (C)
 max_wind_vel = max_wind_vel*0.44704; % maximum allowable wind speed, (m/s)
+
+
 
